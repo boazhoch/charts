d9881b6a671e67ef83ffd93a8c177ac40d0b538f
Boaz Hoch <boazmier@gmail.com>
2019-03-01 11:09:55 +0200

    feat(): Default paramters

--- src/services/stock/index.ts
+++ src/services/stock/index.ts
@@ -1,65 +1,83 @@
 import { IHttp } from "../http/IHttp";
-import { IStockQueryOptions, QUERY_FUNCTIONS, QUERY_INTERVALS, QUERY_OUTPUT_SIZE, IStockService } from "./IStockService";
+import {
+  IStockQueryOptions,
+  QUERY_FUNCTIONS,
+  QUERY_INTERVALS,
+  QUERY_OUTPUT_SIZE,
+  IStockService
+} from "./IStockService";
 
-class StockService implements IStockService { 
-    private requester: IHttp;
-    constructor(requester: IHttp) { 
-        this.requester = requester;
+class StockService implements IStockService {
+  private requester: IHttp;
+  constructor(requester: IHttp) {
+    this.requester = requester;
+  }
+
+  async getStock(options: IStockQueryOptions) {
+    // TODO: handle error and timeouts.
+    const result = await this.requester.get(this.constructQuery(options));
+    const payload = await result.json();
+    const data = payload["Time Series (Daily)"];
+    let datesAndValuesArray = [];
+    for (const date in data) {
+      const dateToTimeStamp: number = new Date(date).getTime();
+      const closeValue: number = parseFloat(data[date]["4. close"]);
+
+      const dateValue = [dateToTimeStamp, closeValue];
+      datesAndValuesArray.push(dateValue);
     }
 
-    async getStock(options: IStockQueryOptions) {
-        const result = await this.requester.get(this.constructQuery(options));
-        const payload = await result.json();
-        const data = payload["Time Series (Daily)"];
-        let datesAndValuesArray = [];
-        for (const date in data) {
-            const dateToTimeStamp: number = new Date(date).getTime();
-            const closeValue: number = parseFloat(data[date]['4. close']);
+    datesAndValuesArray.reverse();
 
-            const dateValue = [dateToTimeStamp, closeValue];
-            datesAndValuesArray.push(dateValue);
-        }
+    return datesAndValuesArray;
+  }
 
-        datesAndValuesArray.reverse();
+  private constructQuery(options: IStockQueryOptions) {
+    const functionType = this.functionQuery(options.function);
+    const symbol = this.symbolQuery(options.symbol);
+    const outputSize = this.outputsizeQuery(options.output);
+    const interval = this.intervalQuery(options.interval);
+    return `/query?${this.chainQueryOptions([
+      functionType,
+      symbol,
+      outputSize,
+      interval
+    ])}&apikey=42ZHNDEL9LV70H59`;
+  }
 
-        return datesAndValuesArray;
-    }
+  private chainQueryOptions(options: string[]) {
+    let queryString: string = "";
 
-    private constructQuery(options: IStockQueryOptions) { 
-        const functionType = this.functionQuery(options.function);
-        const symbol = this.symbolQuery(options.symbol);
-        const outputSize = this.outputsizeQuery(options.output);
-        const interval = this.intervalQuery(options.interval);
-        return `/query?${this.chainQueryOptions([functionType, symbol, outputSize, interval])}&apikey=42ZHNDEL9LV70H59`;
-    }
+    options.forEach(option => {
+      if (option) {
+        queryString = !queryString ? option : `${queryString}&${option}`;
+      }
+    });
 
-    private chainQueryOptions(options: string[]) { 
-        let queryString: string = '';
+    return queryString;
+  }
 
-        options.forEach((option) => {
-            if (option) {
-                queryString = !queryString ? option : `${queryString}&${option}`;
-            }
-        });
+  private functionQuery(
+    functionType: QUERY_FUNCTIONS = QUERY_FUNCTIONS.TIME_SERIES_DAILY
+  ) {
+    return `function=${functionType}`;
+  }
 
-        return queryString;
-    }
+  private symbolQuery(symbol: string) {
+    return `symbol=${symbol}`;
+  }
 
-    private functionQuery(functionType?: QUERY_FUNCTIONS) { 
-        return `function=${functionType || QUERY_FUNCTIONS.TIME_SERIES_DAILY}`;
-    }
+  private intervalQuery(
+    intervalType: QUERY_INTERVALS = QUERY_INTERVALS["5MIN"]
+  ) {
+    return `interval=${intervalType}`;
+  }
 
-    private symbolQuery(symbol: string) {
-        return `symbol=${symbol}`;
-    }
-
-    private intervalQuery(intervalType?: QUERY_INTERVALS) {
-        return `interval=${intervalType || QUERY_INTERVALS["5MIN"]}`;
-    }
-
-    private outputsizeQuery(outputType?: QUERY_OUTPUT_SIZE) {
-        return `outputsize=${outputType || QUERY_OUTPUT_SIZE.COMPACT}`;
-    }
+  private outputsizeQuery(
+    outputType: QUERY_OUTPUT_SIZE = QUERY_OUTPUT_SIZE.COMPACT
+  ) {
+    return `outputsize=${outputType}`;
+  }
 }
 
 export default StockService;
